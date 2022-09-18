c705a66d344d6a9f362202518c1792ade0f79eac
komplektan <komplektan@mail.ru>
2022-09-18 22:59:02 +0500

    Start and Finish

--- FaceRecogniger/ViewController.swift
+++ FaceRecogniger/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        guard let image = UIImage(named: "photo") else { return }
+        
+        let imageView = UIImageView(image: image)
+        let scaleHeight = view.frame.width / image.size.width * image.size.height
+        imageView.contentMode = .scaleAspectFit
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: scaleHeight)
+        view.addSubview(imageView)
+        
+        let request = VNDetectFaceRectanglesRequest { request, error in
 
+    request.usesCPUOnly = true
+  
+            if error != nil {
+                print("ERROR")
+                return
+            }
+            request.results?.forEach({ res in
+                DispatchQueue.main.async {
+                    print(res)
+                }
+            })
+        }
+        
+        guard let cgImage = image.cgImage else { return }
+        
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        
+        do {
+            try handler.perform([request])
+        } catch let reqError {
+            print(reqError)
+        }
+
+    }
 
 }
 
